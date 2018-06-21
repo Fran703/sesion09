0be2781272dabd36688b7b844539fe5ca5743cda
Francisco Javier Marques Gaona <fmg703@inlumine.ual.es>
2018-06-21 16:23:02 +0200

    Cambiando footer

--- HM2018/inc/inc.footer.php
+++ HM2018/inc/inc.footer.php
@@ -1,4 +1,4 @@
 <div class="container">
-  <small>Equipo4. Procesos de Ingenieria Software 1 UAL.</small>
-  <!--<small class="pull-right">Hecho con <i class="fa fa-heart text-rojo"></i> desde Almería.</small>-->
+  <small>Equipo NEYMARDOWN HMIS2018</small>
+  <small class="pull-right">Hecho con <i class="fa fa-heart text-rojo"></i> desde Almería.</small>
 </div>
